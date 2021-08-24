8726ae1482b614c9c9cdf7b3f5230a323db58678
null <goodhorsezxj@gmail.com>
2021-08-24 23:45:52 +0800

    to: upgrade license

--- wax/utils.py
+++ wax/utils.py
@@ -1,7 +1,7 @@
 """
 Copyright (c) 2021  Chongqing Parsec Inc.
-wax-backend is licensed under the Lesspl Public License(v0.2).
-You may obtain a copy of Lesspl Public License(v0.2) at: http://www.lesspl.org
+wax-backend is licensed under the Lesspl Public License(v0.3).
+You may obtain a copy of Lesspl Public License(v0.3) at: http://www.lesspl.org
 """
 
 
